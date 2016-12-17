d9fe7721ba6482590ee3f6bba68bdf42da5de9f3
Sam Lucas <smlucas13@gmail.com>
2016-12-17 11:44:44 -0500

    save

--- joust.js
+++ joust.js
@@ -2,20 +2,34 @@
 var C = {
   //background
   background: {
-    image: 'joustmap.gif',
+    image: 'joustmap.png',
     scale: 1
   },
   //player
   player: {
-    image: 'knight1.png',
-    width: 64,
-    height: 64,
-    frames: 1,
-    startx: 1500,
-    starty: 200,
+    image: 'knightred.gif',
+    width: 32,
+    height: 32,
+    frames: 2,
+    startx: 1400,
+    starty: 300,
     bounce: 0.3,
-    drag: 3000,
-    speed: 300
+    drag: 100,
+    speed: 300,
+    scale: 5
+  },
+  //enemy
+  enemy: {
+    image: 'knightblue.gif',
+    width: 32,
+    height: 32,
+    frames: 2,
+    startx: 100,
+    starty: 430,
+    bounce: 0.3,
+    drag: 100,
+    speed: 300,
+    scale: 5
   },
   
 };
@@ -52,14 +66,11 @@ class StartState {
   }
 
   preload() {
+    this.load.spritesheet('player','knightred.png',32,32,2)
     this.load.image('background',C.background.image);
-    
-    this.load.spritesheet('player',
-      C.player.image,
-      C.player.width,
-      C.player.height,
-      C.player.frames
-    );
+    this.load.image('player',C.player.image);
+    this.load.image('enemy',C.enemy.image);
+    this.load.spritesheet('enemy','knightblue.png',32,32,2)
 
   }
 
@@ -72,78 +83,41 @@ class StartState {
 class PlayState {
 
   create() {
+    
     game.physics.startSystem(Phaser.Physics.P2JS);
     // background
-    this.background = this.add.tileSprite(0,0,1600,1600,'background');
+    this.background = this.add.tileSprite(0,0,1600,600,'background');
     this.game.world.setBounds(0, 0, 1600, 600);
-    //this.background.autoScroll(0,C.background.scroll);
     this.background.scale.set(C.background.scale);
     
     // player
     this.player = game.add.sprite(C.player.startx,C.player.starty,'player');
-    game.physics.p2.enable(this.player);
-    this.player.enableBody
+    this.player.scale.set(C.player.scale);
+    game.physics.arcade.enable(this.player);
+    //this.player.enableBody
     this.player.body.kinematic = true;
+    this.player.body.drag.setTo(C.player.drag);
+    this.player.animations.add('blink');
+    this.player.animations.play('blink',2,true);
+    game.input.onDown.add(move, this.player);
+    // enemy
+    this.enemy = game.add.sprite(C.enemy.startx,C.enemy.starty,'enemy');
+    this.enemy.scale.set(C.enemy.scale);
+    game.physics.arcade.enable(this.enemy);
+    this.enemy.body.kinematic = true;
+    this.enemy.body.drag.setTo(C.enemy.drag);
+    this.enemy.animations.add('blink');
+    this.enemy.animations.play('blink',2,true);
+    this.enemy.smoothed = false;
+
     game.physics.p2.updateBoundsCollisionGroup();
     this.player.body.fixedRotation = true;
     console.log(this.player);
-    this.game.camera.follow(this.player);
+    this.player.smoothed = false;
     
-    
-    //Add keys
-    this.right = game.input.keyboard.addKey(Phaser.KeyCode.D);
-    this.left = game.input.keyboard.addKey(Phaser.KeyCode.A);
-    this.up = game.input.keyboard.addKey(Phaser.KeyCode.W);
-    this.down = game.input.keyboard.addKey(Phaser.KeyCode.S);
   }
 
   update() {
-    
-    //Movementv
-    this.player.body.velocity.x = 0;
-    this.player.body.velocity.y = 0;
-    
-    if (this.left.isDown) {
-        this.player.body.moveLeft(50);
-        this.player.angle = 270;
-    }
-    else if (this.right.isDown) {
-        this.player.body.moveRight(50);
-        this.player.angle = 90;
-    }
-    if (this.up.isDown) {
-        this.player.body.moveUp(50);
-        this.player.angle = 0;
-    }
-    else if (this.down.isDown) {
-        this.player.body.moveDown(50);
-        this.player.angle = 180;
-    }
-    //angles
-    if (this.up.isDown && this.right.isDown) {
-        this.player.angle = 40;
-    }
-    else if (this.up.isDown && this.left.isDown) {
-        this.player.angle = 320;
-    }
-    if (this.down.isDown && this.right.isDown) {
-        this.player.angle = 140;
-    }
-    else if (this.down.isDown && this.left.isDown) {
-        this.player.angle = 220;
-    }
-    //Movement^
-    
-    //camera
-    if (this.game.camera.atLimit.x)
-    {
-        this.background.tilePosition.x -= (this.player.body.velocity.x * game.time.physicsElapsed);
-    }
-
-    if (this.game.camera.atLimit.y)
-    {
-        this.background.tilePosition.y -= (this.player.body.velocity.y * game.time.physicsElapsed);
-    }
 }
 }
 class EndState {
@@ -153,7 +127,11 @@ class EndState {
 }
 
 
-var game = new Phaser.Game(800,800);
+function move() {
+  console.log(this);
+  this.body.velocity.x -= 50;
+}
+var game = new Phaser.Game(1600,800);
 game.state.add('Boot', BootState);
 game.state.add('Preload', PreloadState);
 game.state.add('Start', StartState);
